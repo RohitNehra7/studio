0eb0ca43622e09f6c7147b0b2288f52c93a3cfb0
Rohit Nehra <rnehra2631997@gmail.com>
2025-07-30 18:28:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/home/our-approach-section.tsx
+++ src/components/home/our-approach-section.tsx
@@ -47,7 +47,7 @@ export function OurApproach() {
                 </div>
                 <div className="order-1 md:order-2 rounded-lg overflow-hidden shadow-2xl group">
                     <Image
-                        src="https://images.unsplash.com/photo-1606310967084-6f1b21596945?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxhcHByb2FjaHxlbnwwfHx8fDE3NTM4OTk5NTV8MA&ixlib=rb-4.1.0&q=80&w=1080"
+                        src="https://images.unsplash.com/photo-1692129970987-da3aba03eeb7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxhcHByb2FjaHxlbnwwfHx8fDE3NTM4OTk5NTV8MA&ixlib=rb-4.1.0&q=80&w=1080"
                         alt="Interior of a modern building"
                         width={800}
                         height={600}
