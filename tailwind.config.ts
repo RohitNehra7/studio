44c932bb5943d91b9fd77c284baccc6bd2a17de8
Rohit Nehra <rnehra2631997@gmail.com>
2025-07-30 17:47:35 +0000

    the zoom in effect should be a little more and it should be continuous n

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,15 @@ export default {
             height: '0',
           },
         },
+        'zoom-in-out': {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.15)' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'zoom-in-out': 'zoom-in-out 20s ease-in-out infinite',
       },
     },
   },
