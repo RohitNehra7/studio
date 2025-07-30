a0bb3d392d446db1f3992f9cca9e50fddc4da259
Rohit Nehra <rnehra2631997@gmail.com>
2025-07-30 20:10:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -6,7 +6,7 @@ import logo from "@/assets/homepage/logo.jpeg";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-3", className)}>
-      <Image src={logo} alt="The Archithan Architects Logo" width={40} height={40} />
+      <Image src={logo} alt="The Archithan Architects Logo" width={50} height={50} />
     </div>
   );
 }
